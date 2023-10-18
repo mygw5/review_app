bf0e00c387cf52daacd826a0a7933ad377d8342d
mygw5 <135333208+mygw5@users.noreply.github.com>
2023-10-18 02:23:49 +0000

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,5 @@
 Rails.application.routes.draw do
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
-  namespace :public do
-    get 'comments/create'
-    get 'comments/destroy'
-  end
-  namespace :public do
-    get 'cosmetics/index'
-    get 'cosmetics/edit'
-    get 'cosmetics/show'
-    get 'cosmetics/favorite'
-  end
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
   devise_for :admins, controllers: {
@@ -23,6 +10,33 @@ Rails.application.routes.draw do
   registrations: "public/registrations",
     sessions: 'public/sessions'
   }
+
+  scope module: :public do
+    root to: "homes#top"
+    get "about" => "homes#about"
+    resources :cosmetics, only: [:index, :show, :edit, :create, :update, :destroy] do
+      get "mypost"
+      get "mylist"
+      get "draft"
+    end
+
+    resources :end_users, only: [:show, :update] do
+      get "mypage"
+      get "confirm_withdraw"
+      patch "withdraw"
+    end
+
+    get "notice" => "nofications#notice"
+  end
+
+  namespace :admin do
+    root to: "homes#top"
+    resources :cosmetics, only: [:index, :show, :edit, :update, :destroy]
+    resources :categories, only: [:index, :create, :update, :destroy]
+    resources :end_users, only: [:index, :show, :edit, :update]
+    get "notice" => "nofications#notice"
+  end
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
