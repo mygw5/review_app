053ede57f1abb9df58b5c50a3700fd57428004b7
mygw5 <135333208+mygw5@users.noreply.github.com>
2023-10-21 11:54:36 +0000

    Add ユーザーコントローラー

--- app/controllers/public/end_users_controller.rb
+++ app/controllers/public/end_users_controller.rb
@@ -1,11 +1,26 @@
 class Public::EndUsersController < ApplicationController
   before_action :authenticate_end_user!
+  before_action :set_user
+
+  def show
+  end
+
+  def update
+  end
 
   def mypage
+  end
 
+  def confirm_withdraw
+  end
+
+  def withdraw
   end
 
   private
 
+  def set_user
+    @end_user = EndUser.find(params[:id])
+  end
 
 end
